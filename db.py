e00c52ceac12b0484973335f49aa331657858e6e
Brwni <brianmm665@gmial.com>
2023-09-07 12:23:33 +0300

    Registry

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 # The engine & the base declarative class
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,3 +12,15 @@ session = DBSession()
 
 # Creating base declarative model class
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100))
+    
+class Subject(Base):
+    __tablename__ = 'subjects'
+    
+    id = Column(Integer, primary_key=True)
+    name = (String)
--- education_app.py
+++ education_app.py
@@ -1,6 +1,26 @@
 # Logics
-from db import Base, engine
+import click
+from db import Base, engine, User, Subject
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+@click.command()
+@click.option('--username', prompt='PLease enter your username')
+def register(username):
+    # Checking if the user already exists
+    user = session.query(User).filter_by(username=username).first()
+    if user:
+        click.echo(f'Username {username} is already taken. Please choose a different username.')
+    else:
+        # Creating a new user and adding them to the database
+        new_user = User(username=username)
+        session.add(new_user)
+        session.commit()
+        click.echo(f'User {username} registered successfully!')
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    print("You've Passed!")
+    register()
+    select_subject()
